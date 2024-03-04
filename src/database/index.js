2b208376e856eaeff44cd64e8f689900baa9adbe
SSoyBoy <114915560+SSoyBoy@users.noreply.github.com>
2024-03-04 15:56:14 +0700

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,38 @@
+// import mongoose from "mongoose";
+
+// const configOptions = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
+
+// const connectToDB = async () => {
+//   const connectionUrl = process.env.NEXT_PUBLIC_MONGODB_URL;
+
+//   mongoose
+//     .connect(connectionUrl, configOptions)
+//     .then(() => console.log("Ecommerce database connected successfully!"))
+//     .catch((err) =>
+//       console.log(`Getting Error from DB connection ${err.message}`)
+//     );
+// };
+
+// export default connectToDB;
+
 import mongoose from "mongoose";
 
-const configOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
 const connectToDB = async () => {
-  const connectionUrl = process.env.NEXT_PUBLIC_MONGODB_URL;
+  try {
+    const connectionUrl = process.env.NEXT_PUBLIC_MONGODB_URL;
 
-  mongoose
-    .connect(connectionUrl, configOptions)
-    .then(() => console.log("Ecommerce database connected successfully!"))
-    .catch((err) =>
-      console.log(`Getting Error from DB connection ${err.message}`)
-    );
+    await mongoose.connect(connectionUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Ecommerce database connected successfully!");
+  } catch (err) {
+    console.error(`Error from DB connection: ${err.message}`);
+  }
 };
 
 export default connectToDB;
